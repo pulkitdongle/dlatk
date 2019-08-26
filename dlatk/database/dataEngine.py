307726472ada9672418bc3ae88bfebd9c5cf627c
Pulkit Dongle <pulkit@wwbp.sas.upenn.edu>
2019-08-25 22:19:23 -0400

    Updated documentation

--- dlatk/database/dataEngine.py
+++ dlatk/database/dataEngine.py
@@ -3,53 +3,108 @@ from .. import dlaConstants as dlac
 
 
 class DataEngine(object):
-    def __init__(self, corpdb=dlac.DEF_CORPDB, sql_host=dlac.MYSQL_HOST, encoding=dlac.DEF_ENCODING,
-                 use_unicode=dlac.DEF_UNICODE_SWITCH, db_type=dlac.DB_TYPE):
-        self.encoding = encoding
-        self.sql_host = sql_host
-        self.corpdb = corpdb
-        self.use_unicode = use_unicode
-        self.db_type = db_type
-        self.dataEngine = None
+	"""
+	Class for connecting with the database engine (based on the type of data engine being used) and executing querie.s
 
-    def connect(self):
-        if self.db_type == "mysql":
-            self.dataEngine = MySqlDataEngine(self.corpdb, self.sql_host, self.encoding)
-            return self.dataEngine.get_db_connection()
-        if self.db_type == "sqlite":
-            # instantiate SqliteDataEngine creating the connection with database and return its object
-            pass
+	Parameters
+	-------------
+	corpdb: str
+		Corpus Database name.
+	sql_host: str
+		Host that the mysql server runs on.
+	encoding: str
+		MySQL encoding
+	db_type: str
+		Type of the database being used (mysql, sqlite).
+	
+	"""
+	def __init__(self, corpdb=dlac.DEF_CORPDB, sql_host=dlac.MYSQL_HOST, encoding=dlac.DEF_ENCODING,use_unicode=dlac.DEF_UNICODE_SWITCH, db_type=dlac.DB_TYPE):
+		self.encoding = encoding
+		self.sql_host = sql_host
+		self.corpdb = corpdb
+		self.use_unicode = use_unicode
+		self.db_type = db_type
+		self.dataEngine = None
 
-    def disable_table_keys(self, featureTableName):
-        self.dataEngine.disable_table_keys(featureTableName)
+	def connect(self):
+		"""
+		Establishes connection with the database engine
+	
+		Returns
+		-------------
+		Database connection objects
+		"""
+		if self.db_type == "mysql":
+			self.dataEngine = MySqlDataEngine(self.corpdb, self.sql_host, self.encoding)
+			return self.dataEngine.get_db_connection()
+		if self.db_type == "sqlite":
+			# instantiate SqliteDataEngine creating the connection with database and return its object
+			pass
 
-    def enable_table_keys(self, featureTableName):
-        self.dataEngine.enable_table_keys(featureTableName)
+	def disable_table_keys(self, featureTableName):
+		"""
+		Disable keys: good before doing a lot of inserts.
+		"""
+		self.dataEngine.disable_table_keys(featureTableName)
 
-    def execute_get_list(self, usql):
-        return self.dataEngine.execute_get_list(usql)
+	def enable_table_keys(self, featureTableName):
+		"""
+		Enables the keys, for use after inserting (and with keys disabled)
+		"""
+		self.dataEngine.enable_table_keys(featureTableName)
+
+	def execute_get_list(self, usql):
+		"""
+		Executes a given query, returns results as a list of lists
+		"""
+		return self.dataEngine.execute_get_list(usql)
 
 
 
 class MySqlDataEngine(DataEngine):
+	"""
+	Class for interacting with the MYSQL database engine.
+	Parameters
+	------------
+	corpdb: str
+		Corpus database name.
+	mysql_host: str
+		Host that the mysql server runs on
+	encoding: str
+		MYSQL encoding
+	"""
 
-    def __init__(self, corpdb, mysql_host, encoding):
-        super().__init__()
-        (self.dbConn, self.dbCursor, self.dictCursor) = mm.dbConnect(corpdb, host=mysql_host, charset=encoding)
+	def __init__(self, corpdb, mysql_host, encoding):
+		super().__init__()
+		(self.dbConn, self.dbCursor, self.dictCursor) = mm.dbConnect(corpdb, host=mysql_host, charset=encoding)
 
-    def get_db_connection(self):
-        return self.dbConn, self.dbCursor, self.dictCursor
+	def get_db_connection(self):
+		"""
+		Returns
+		------------
+		Database connection objects
+		"""
+		return self.dbConn, self.dbCursor, self.dictCursor
 
-    def execute_get_list(self, usql):
-        return mm.executeGetList(self.corpdb, self.dbCursor, usql, charset=self.encoding, use_unicode=self.use_unicode)
+	def execute_get_list(self, usql):
+		"""
+		Executes a given query, returns results as a list of lists
+		"""
+		return mm.executeGetList(self.corpdb, self.dbCursor, usql, charset=self.encoding, use_unicode=self.use_unicode)
 
-    def disable_table_keys(self, featureTableName):
-        mm.disableTableKeys(self.corpdb, self.dbCursor, featureTableName, charset=self.encoding, use_unicode=self.use_unicode)
+	def disable_table_keys(self, featureTableName):
+		"""
+		Disable keys: good before doing a lot of inserts.
+		"""
+		mm.disableTableKeys(self.corpdb, self.dbCursor, featureTableName, charset=self.encoding, use_unicode=self.use_unicode)
 
-    def enable_table_keys(self, featureTableName):
-        mm.enableTableKeys(self.corpdb, self.dbCursor, featureTableName, charset=self.encoding, use_unicode=self.use_unicode)
+	def enable_table_keys(self, featureTableName):
+		"""
+		Enables the keys, for use after inserting (and with keys disabled)
+		"""
+		mm.enableTableKeys(self.corpdb, self.dbCursor, featureTableName, charset=self.encoding, use_unicode=self.use_unicode)
 
 class SqliteDataEngine(DataEngine):
-    # contains methods similar to MySqlWrapper class
-    # these methods will call methods in mysqliteMethods.py (yet to be created) which will be similar to mysqlMethods.py
-    pass
+	# will contain methods similar to MySqlWrapper class
+	# these methods will call methods in mysqliteMethods.py (yet to be created) which will be similar to mysqlMethods.py
+	pass
--- dlatk/database/query.py
+++ dlatk/database/query.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 from .dataEngine import DataEngine
-#from .. import dlaConstants as dlac
 
 class Query(ABC):
+	"""
+	Abstract class. Base class for the different query building classes like SelectQuery
+	"""
 	def __init__(self):
 		pass
-		#self.sqlType = dlac.DB_TYPE
 
 	@abstractmethod
 	def build_query(self):
@@ -17,16 +18,43 @@ class Query(ABC):
 
 
 class QueryBuilder():
+	"""
+	This class is directly used by DLATK classes for building queries.
 
-#should have the dataEngine name
+	Parameters
+	------------
+	data_engine: str
+		Name of the data engine eg. mysql, sqlite
+	"""
+
 	def __init__(self, data_engine):
 		self.data_engine = data_engine
 
 	def create_select_query(self,from_table):
+		"""
+		Parameters
+		------------
+		from_table: str
+			Name of the table from which records are to be fetched.
+		
+		Returns
+		------------
+		Object of SelectQuery class	
+		"""
 		return SelectQuery(from_table, self.data_engine)
 
 
 class SelectQuery(Query):
+	"""
+	Class for building a SELECT query.
+
+	Parameters
+	------------
+	from_table: str
+		Name of the table from which records are to be fetched.
+	data_engine: str
+		Name of the database engine being used.
+	"""
 
 	def __init__(self, from_table, data_engine):
 		super().__init__()
@@ -37,21 +65,52 @@ class SelectQuery(Query):
 		self.data_engine = data_engine
 
 	def set_fields(self, fields):
+		"""
+		Parameters
+		------------
+		fields: list
+			List containing name of the columns
+	
+		Returns	
+		------------
+		SelectQuery object		
+		"""
 		self.fields = fields
 		return self
 
 	def group_by(self, group_by_fields):
+		"""
+		Parameters
+		------------
+		group_by_fields: list
+			List containing name of the fields which should be used for grouping.
+	
+		Returns	
+		------------
+		SelectQuery object		
+		"""
 		self.group_by_fields = group_by_fields
 		return self
 
 	def execute_query(self):
+		"""
+		Executes sql query
+	
+		Returns
+		------------
+		List of lists
+		"""
 		self.sql = self.build_query()
-		print("********************************************")
-		print(self.__dict__)
-		print("********************************************")
 		return self.data_engine.execute_get_list(self.sql)
 
 	def build_query(self):
+		"""
+		Builds a sql query based on the type of db
+	
+		Returns
+		------------
+		str: a built select query
+		"""
 		if self.data_engine.db_type == "mysql":
 			return """SELECT %s FROM %s GROUP BY %s""" %(','.join(self.fields), self.from_table, ','.join(self.group_by_fields))
 
